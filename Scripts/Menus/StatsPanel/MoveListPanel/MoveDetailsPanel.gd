48bb15a37c2598a2c4f2e5fd1656ad9a227ae550
123outerme <123outerme.outlook.com>
2023-10-09 16:18:04 -0400

    Completed move details panel except status effect stuff

--- Scripts/Menus/StatsPanel/MoveListPanel/MoveDetailsPanel.gd
+++ Scripts/Menus/StatsPanel/MoveListPanel/MoveDetailsPanel.gd
@@ -6,11 +6,11 @@ signal back_pressed
 @export var move: Move = null
 
 @onready var moveName: RichTextLabel = get_node('Panel/MoveName')
+@onready var movePower: RichTextLabel = get_node("Panel/MovePower")
+@onready var damageCategory: RichTextLabel = get_node("Panel/DamageCategory")
+@onready var moveTargets: RichTextLabel = get_node("Panel/MoveTargets")
 @onready var requiredLv: RichTextLabel = get_node("Panel/RequiredLv")
 @onready var moveRole: RichTextLabel = get_node("Panel/MoveRole")
-@onready var movePower: RichTextLabel = get_node("Panel/MovePower")
-@onready var moveCategory: RichTextLabel = get_node("Panel/DamageCategory")
-@onready var moveTargets: RichTextLabel = get_node("Panel/MoveTargets")
 @onready var moveStatChanges: RichTextLabel = get_node("Panel/MoveStatChanges")
 @onready var moveStatusEffect: RichTextLabel = get_node("Panel/MoveStatusEffect")
 @onready var moveStatusChance: RichTextLabel = get_node("Panel/MoveStatusChance")
@@ -27,8 +27,24 @@ func load_move_details_panel():
 	
 	visible = true
 	moveName.text = '[center]' + move.moveName + '[/center]'
-	requiredLv.text = 'Required Level: ' + str(move.requiredLv)
-	moveRole.text = '[center]' + Move.role_to_string(move.role) + '[/center]'
+	movePower.text = str(move.power) + ' Power'
+	damageCategory.text = Move.dmg_category_to_string(move.category)
+	moveTargets.text = '[center]Targets ' + BattleCommand.targets_to_string(move.targets) + '[/center]'
+	requiredLv.text = '[right]Required Level: ' + str(move.requiredLv) + '[/right]'
+	moveRole.text = '[right]' + Move.role_to_string(move.role) + '[/right]'
+	moveStatChanges.text = ''
+	var multipliers: Array[StatMultiplierText] = move.statChanges.get_multipliers_text()
+	for idx in range(len(multipliers)):
+		moveStatChanges.text += multipliers[idx].print_multiplier()
+		if idx < len(multipliers) - 1:
+			if len(multipliers) > 2:
+				moveStatChanges.text += ','
+			if idx == len(multipliers) - 2:
+				moveStatChanges.text += ' and'
+			moveStatChanges.text += ' '
+	#moveStatusEffect.text = ''
+	#moveStatusChance.text = ''
+	moveDescription.text = move.description
 	
 func _on_back_button_pressed():
 	visible = false
