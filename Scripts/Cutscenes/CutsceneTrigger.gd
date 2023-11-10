34d61ec0393fa29ee6e0754f85bc30e31ff48223
123outerme <123outerme.outlook.com>
2023-11-09 21:13:24 -0500

    Improve target selected sprites

--- Scripts/Cutscenes/CutsceneTrigger.gd
+++ Scripts/Cutscenes/CutsceneTrigger.gd
@@ -20,7 +20,6 @@ func _ready():
 func _process(delta):
 	if playing and not disabled and not isPaused:
 		timer += delta
-		
 		var frame: CutsceneFrame = cutscene.get_keyframe_at_time(timer)
 		
 		if PlayerFinder.player.is_in_dialogue() and lastFrame != null and lastFrame.endTextBoxPauses:
