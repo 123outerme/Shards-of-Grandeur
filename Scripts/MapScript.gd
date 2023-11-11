1380bf347df68982199d8cd3402d5eb9ccf1e858
123outerme <123outerme.outlook.com>
2023-11-11 16:42:08 -0500

    Improved dual layer tile assigning script

--- Scripts/MapScript.gd
+++ Scripts/MapScript.gd
@@ -2,27 +2,66 @@
 extends Node2D
 class_name MapScript
 
+class TileDef:
+	var layer: int = 0
+	var atlasId: int = 0
+	var atlasPos: Vector2i = Vector2i()
+	var altTileId: int = 0
+	
+	func _init(
+		i_layer = 0,
+		i_atlasId = 0,
+		i_atlasPos = Vector2i(),
+		i_altTile = 0
+	):
+		layer = i_layer
+		atlasId = i_atlasId
+		atlasPos = i_atlasPos
+		altTileId = i_altTile
+
 signal map_loaded
 
-@export var autoAssignTreeTops: bool:
+@export var autoAssignTwoLayerTiles: bool:
 	get:
 		return false
 	set(value):
-		_assign_tree_tops(value)
+		_assign_two_layer_tiles(value)
 
 func _ready():
 	map_loaded.emit()
 
-func _assign_tree_tops(booleanVal: bool):
+func _assign_two_layer_tiles(booleanVal: bool):
 	if Engine.is_editor_hint():
+		print('Matched two-layer terrain tiles')
+		var bottomTiles: Array[TileDef] = [
+			TileDef.new(1, 1, Vector2i(0,1)),
+			TileDef.new(1, 1, Vector2i(1,1)),
+			TileDef.new(1, 1, Vector2i(2,1)),
+			TileDef.new(1, 2, Vector2i(0,1)),
+			TileDef.new(1, 2, Vector2i(1,1)),
+			TileDef.new(1, 2, Vector2i(2,1)),
+		]
+		var topTiles: Array[TileDef] = [
+			TileDef.new(2, 1, Vector2i(0,0)),
+			TileDef.new(2, 1, Vector2i(1,0)),
+			TileDef.new(2, 1, Vector2i(2,0)),
+			TileDef.new(2, 2, Vector2i(0,0)),
+			TileDef.new(2, 2, Vector2i(1,0)),
+			TileDef.new(2, 2, Vector2i(2,0)),
+		]
+		var deltas: Array[Vector2i] = [
+			Vector2i(0, -1),
+			Vector2i(0, -1),
+			Vector2i(0, -1),
+			Vector2i(0, -1),
+			Vector2i(0, -1),
+			Vector2i(0, -1),
+		]
+		
 		var tilemap: TileMap = get_node("TileMap")
-		for tree1Tile in tilemap.get_used_cells_by_id(1, 1, Vector2i(0,1)):
-			var tree1TopTile: Vector2i = Vector2i(tree1Tile.x, tree1Tile.y - 1)
-			tilemap.set_cell(2, tree1TopTile, 1, Vector2i(0, 0))
-		for tree2Tile in tilemap.get_used_cells_by_id(1, 1, Vector2i(1,1)):
-			var tree2TopTile: Vector2i = Vector2i(tree2Tile.x, tree2Tile.y - 1)
-			tilemap.set_cell(2, tree2TopTile, 1, Vector2i(1, 0))
-		for tree3Tile in tilemap.get_used_cells_by_id(1, 1, Vector2i(2,1)):
-			var tree3TopTile: Vector2i = Vector2i(tree3Tile.x, tree3Tile.y - 1)
-			tilemap.set_cell(2, tree3TopTile, 1, Vector2i(2, 0))
-	pass
+		
+		for idx in len(bottomTiles):
+			for tilePos in tilemap.get_used_cells_by_id(bottomTiles[idx].layer, bottomTiles[idx].atlasId, bottomTiles[idx].atlasPos, bottomTiles[idx].altTileId):
+				var topTilePos: Vector2i = Vector2i(tilePos.x + deltas[idx].x, tilePos.y + deltas[idx].y)
+				tilemap.set_cell(topTiles[idx].layer, topTilePos, topTiles[idx].atlasId, topTiles[idx].atlasPos, topTiles[idx].altTileId)
+		pass
