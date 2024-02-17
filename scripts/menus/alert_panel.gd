15aae2f79f814ddc98b88a0681036d3588520ff3
123outerme <123outerme.outlook.com>
2024-02-17 17:39:18 -0500

    Change alert sfx to be distinct from textbox skip; polished alert animation

--- scripts/menus/alert_panel.gd
+++ scripts/menus/alert_panel.gd
@@ -1,7 +1,7 @@
 extends Panel
 class_name AlertPanel
 
-const TWEEN_ONSCREEN_TIME = 1.25
+const TWEEN_ONSCREEN_TIME = 0.65
 const TWEEN_OFFSCREEN_TIME = 1.25
 
 @export var message: String = ''
@@ -16,19 +16,18 @@ var lifetimeAccum: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	panelTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_LINEAR)
+	panelTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	position = Vector2(0, -150)
 	messageLabel.text = '[center]' + message + '[/center]'
 	panelTween.tween_property(self, 'position', Vector2(0, 0), TWEEN_ONSCREEN_TIME)
 	panelTween.finished.connect(_show_finished)
-	SceneLoader.audioHandler.play_sfx(alertSfx)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not pauseTimer and position == Vector2(0, 0) and panelTween == null:
 		lifetimeAccum += delta
 		if lifetimeAccum > lifetime:
-			panelTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_LINEAR)
+			panelTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 			panelTween.tween_property(self, 'position', Vector2(0, -150), TWEEN_OFFSCREEN_TIME)
 			panelTween.finished.connect(_hide_finished)
 
@@ -40,6 +39,7 @@ func resume_timer():
 
 func _show_finished():
 	panelTween = null
+	SceneLoader.audioHandler.play_sfx(alertSfx)
 
 func _hide_finished():
 	queue_free()
