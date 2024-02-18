eed2ae80fb7be76fd17627939ca984b3fa05c820
123outerme <123outerme.outlook.com>
2024-02-17 19:40:50 -0500

    Add item sprite to equipped combatant item in equip panel; remove icon from splash screen

--- scripts/menus/inventory_panel/equip_combatant_panel.gd
+++ scripts/menus/inventory_panel/equip_combatant_panel.gd
@@ -8,6 +8,7 @@ var parentPanel: EquipPanel = null
 
 @onready var animatedCombatantSprite: AnimatedSprite2D = get_node('SpriteControl/CombatantSprite')
 @onready var combatantName: RichTextLabel = get_node('CenterCombatantName/CombatantName')
+@onready var itemSprite: Sprite2D = get_node('ItemSpriteControl/ItemSprite')
 @onready var statsButton: Button = get_node('CenterButtons/HBoxContainer/StatsButton')
 @onready var equipButton: Button = get_node('CenterButtons/HBoxContainer/EquipButton')
 @onready var unequipButton: Button = get_node('CenterButtons/HBoxContainer/UnequipButton')
@@ -36,6 +37,9 @@ func load_equip_combatant_panel():
 	
 	animatedCombatantSprite.sprite_frames = combatant.spriteFrames
 	animatedCombatantSprite.play('walk')
+	
+	itemSprite.texture = item.itemSprite
+	itemSprite.visible = unequipButton.visible
 
 func _on_stats_button_pressed():
 	parentPanel.stats_pressed(combatant)
