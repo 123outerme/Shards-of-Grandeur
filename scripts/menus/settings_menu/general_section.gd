2c5df970d2d16d615b196557210d3913a30ea7d1
123outerme <123outerme.outlook.com>
2024-03-01 11:30:42 -0500

    Improved grandstone buildings; fix framerate setting input issues

--- scripts/menus/settings_menu/general_section.gd
+++ scripts/menus/settings_menu/general_section.gd
@@ -3,13 +3,18 @@ class_name GeneralSection
 
 @export var sectionToggleButton: Button
 
+var framerate: int = 60
+var prevFramerate: int = 60
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var allFramerateTextSelected: bool = false
+
 @onready var virtualKeyboard: VirtualKeyboard = get_node('Control/VirtualKeyboard')
 
 @onready var onscreenKeyboardButton: CheckButton = get_node('Control/VBoxContainer/OnscreenKeyboardControl/OnscreenKeyboardLabel/OnscreenKeyboardButton')
 @onready var screenShakeButton: CheckButton = get_node('Control/VBoxContainer/ScreenShakeControl/ScreenShakeLabel/ScreenShakeButton')
 @onready var runToggleButton: CheckButton = get_node('Control/VBoxContainer/RunToggleControl/RunToggleLabel/RunToggleButton')
 @onready var vsyncButton: CheckButton = get_node('Control/VBoxContainer/VSyncControl/VsyncLabel/VsyncButton')
-@onready var framerateSpinBox: SpinBox = get_node('Control/VBoxContainer/FramerateControl/FramerateLabel/FramerateSpinBox')
+@onready var framerateLineEdit: LineEdit = get_node('Control/VBoxContainer/FramerateControl/FramerateLabel/FramerateLineEdit')
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,19 +22,21 @@ func _ready():
 
 func toggle_section(toggle: bool):
 	visible = toggle
+	framerate = SettingsHandler.gameSettings.framerate
+	prevFramerate = framerate
 	if toggle:
 		onscreenKeyboardButton.button_pressed = SettingsHandler.gameSettings.useVirtualKeyboard
 		screenShakeButton.button_pressed = SettingsHandler.gameSettings.screenShake
 		runToggleButton.button_pressed = SettingsHandler.gameSettings.toggleRun
 		vsyncButton.button_pressed = SettingsHandler.gameSettings.vsync
-		framerateSpinBox.value = SettingsHandler.gameSettings.framerate
+		framerateLineEdit.text = String.num(framerate) + ' FPS'
 		
 		onscreenKeyboardButton.focus_neighbor_left = onscreenKeyboardButton.get_path_to(sectionToggleButton)
 		sectionToggleButton.focus_neighbor_right = sectionToggleButton.get_path_to(onscreenKeyboardButton)
 		screenShakeButton.focus_neighbor_left = screenShakeButton.get_path_to(sectionToggleButton)
 		runToggleButton.focus_neighbor_left = runToggleButton.get_path_to(sectionToggleButton)
 		vsyncButton.focus_neighbor_left = vsyncButton.get_path_to(sectionToggleButton)
-		framerateSpinBox.focus_neighbor_left = framerateSpinBox.get_path_to(sectionToggleButton)
+		framerateLineEdit.focus_neighbor_left = framerateLineEdit.get_path_to(sectionToggleButton)
 
 func _on_onscreen_keyboard_button_toggled(toggled_on):
 	SettingsHandler.gameSettings.useVirtualKeyboard = toggled_on
@@ -47,33 +54,54 @@ func _on_vsync_button_toggled(toggled_on):
 	SettingsHandler.gameSettings.vsync = toggled_on
 	SettingsHandler.gameSettings.apply_vsync()
 
-func _on_framerate_spin_box_value_changed(value):
-	var framerate = min(144, max(30, value)) # bound between 144 and 30
-	
-	SettingsHandler.gameSettings.framerate = framerate
-	SettingsHandler.gameSettings.apply_framerate()
-
 func _on_virtual_keyboard_backspace_pressed():
-	framerateSpinBox.value = floori(framerateSpinBox.value / 10)
+	if allFramerateTextSelected:
+		framerateLineEdit.text = ''
+		allFramerateTextSelected = false
+	else:
+		framerate = floori(framerate / 10)
+		framerateLineEdit.text = String.num(framerate)
 
 func _on_virtual_keyboard_enter_pressed():
-	framerateSpinBox.value = min(144, max(30, framerateSpinBox.value))
-	_on_framerate_spin_box_value_changed(framerateSpinBox.value)
-	vsyncButton.grab_focus()
+	_on_framerate_line_edit_text_submitted(framerateLineEdit.text)
 
 func _on_virtual_keyboard_key_pressed(character):
-	var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 	var charIdx = numbers.find(character)
 	#print(charIdx)
 	if charIdx != -1:
-		var value = framerateSpinBox.value * 10
-		value += charIdx
-		framerateSpinBox.value = value
+		if allFramerateTextSelected:
+			framerate = charIdx
+			allFramerateTextSelected = false
+		else:
+			framerate *= 10
+			framerate += charIdx
+		framerateLineEdit.text = String.num(framerate)
 
 func _on_virtual_keyboard_keyboard_hidden():
-	framerateSpinBox.value = min(144, max(30, framerateSpinBox.value))
-	_on_framerate_spin_box_value_changed(framerateSpinBox.value)
+	framerateLineEdit.text = String.num(framerate) + ' FPS'
 	vsyncButton.grab_focus()
 
-func _on_framerate_spin_box_focus_entered():
-	print('debug focus framerate spin box')
+func _on_framerate_line_edit_focus_entered():
+	framerateLineEdit.text = String.num(framerate)
+	allFramerateTextSelected = true
+
+func _on_framerate_line_edit_focus_exited():
+	_on_framerate_line_edit_text_submitted(framerateLineEdit.text)
+
+func _on_framerate_line_edit_text_changed(new_text: String):
+	if new_text.is_valid_int():
+		framerate = min(144, max(30, new_text.to_int())) # bound between 144 and 30
+		SettingsHandler.gameSettings.framerate = framerate
+
+func _on_framerate_line_edit_text_submitted(new_text):
+	if new_text == '':
+		new_text = '60' # default: 60 FPS
+	if new_text.is_valid_int():
+		framerate = min(144, max(30, new_text.to_int())) # bound between 144 and 30
+	else:
+		framerate = prevFramerate	
+	SettingsHandler.gameSettings.framerate = framerate
+	SettingsHandler.gameSettings.apply_framerate()
+	framerateLineEdit.text = String.num(framerate) + ' FPS'
+	vsyncButton.grab_focus()
+	prevFramerate = framerate
