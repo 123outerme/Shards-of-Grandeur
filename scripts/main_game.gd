394fc8501677813a1ec66dfbda194e3be1ae97cd
123outerme <123outerme.outlook.com>
2024-04-26 10:33:27 -0400

    Fix edit moves panel focus and replace bug; fix all virtual keyboards

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -10,3 +10,11 @@ func _ready():
 	SettingsHandler.gameSettings.apply_window_size(get_viewport())
 	SettingsHandler.gameSettings.apply_fullscreen(get_viewport())
 	SceneLoader.load_main_menu()
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_HIDDEN:
+		# show cursor
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif (event is InputEventJoypadButton or event is InputEventJoypadMotion) and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		# hide cursor
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
