bca3cd073a05b50696715f0fb1fb9546bae37223
123outerme <123outerme.outlook.com>
2024-05-18 18:13:42 -0400

    Add Storm Swarm

--- prefabs/test/query.gd
+++ prefabs/test/query.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	for_all_combatants(print_combatant_weaknesses)
+	for_all_combatants_series([print_combatant_weaknesses, print_combatant_status_resistances])
 	#for_all_combatants(print_combatant_movepool_size)
 	#for_all_combatants(print_combatant_highest_lv_move)
 	print('\n---\n')
@@ -15,6 +15,15 @@ func for_all_combatants(query: Callable):
 		if combatant != null:
 			query.call(combatant)
 
+func for_all_combatants_series(queries: Array[Callable]):
+	var combatantsPath = 'res://gamedata/combatants/'
+	var combatantDirs: PackedStringArray = DirAccess.get_directories_at(combatantsPath)
+	for dir: String in combatantDirs:
+		var combatant: Combatant = Combatant.load_combatant_resource(dir)
+		if combatant != null:
+			for query in queries:
+				query.call(combatant)
+
 func for_all_moves(query: Callable):
 	var movesPath = 'res://gamedata/moves/'
 	var moveDirs: PackedStringArray = DirAccess.get_directories_at(movesPath)
@@ -23,30 +32,101 @@ func for_all_moves(query: Callable):
 		if move != null:
 			query.call(move)
 
+func for_all_moves_series(queries: Array[Callable]):
+	var movesPath = 'res://gamedata/moves/'
+	var moveDirs: PackedStringArray = DirAccess.get_directories_at(movesPath)
+	for dir: String in moveDirs:
+		var move: Move = load(movesPath + dir + '/' + dir + '.tres') as Move
+		if move != null:
+			for query in queries:
+				query.call(move)
+
 func print_combatant_weaknesses(combatant: Combatant):
 	if combatant.moveEffectiveness == null:
 		print(combatant.save_name(), ' has no effectiveness data')
 		return
 	
-	if len(combatant.moveEffectiveness.elementWeaknesses) == 0:
-		print(combatant.save_name(), ' has no weaknesses')
+	if len(combatant.moveEffectiveness.elementWeaknesses) == 0 and len(combatant.moveEffectiveness.elementResistances) == 0:
+		print(combatant.save_name(), ' has no element weaknesses/resistances')
 	else:
-		var printStr: String = combatant.save_name() + ' is weak to '
-		for weakness: Move.Element in combatant.moveEffectiveness.elementWeaknesses:
-			printStr += Move.element_to_string(weakness) + ' '
-		print(printStr)
+		if len(combatant.moveEffectiveness.elementWeaknesses) > 0:
+			var printStr: String = combatant.save_name() + ' is weak to '
+			for weakness: Move.Element in combatant.moveEffectiveness.elementWeaknesses:
+				printStr += Move.element_to_string(weakness) + ' '
+			print(printStr)
+		else:
+			print(combatant.save_name(), ' has no element weaknesses')
+		if len(combatant.moveEffectiveness.elementResistances) > 0:
+			var printStr: String = combatant.save_name() + ' element resists '
+			for resistance: Move.Element in combatant.moveEffectiveness.elementResistances:
+				printStr += Move.element_to_string(resistance) + ' '
+			print(printStr)
+		else:
+			print(combatant.save_name(), ' has no element resistances')
 	if combatant.evolutions != null:
 		for evolution: Evolution in combatant.evolutions.evolutionList:
 			if evolution.moveEffectiveness == null:
 				print(evolution.evolutionSaveName, ' has no effectiveness data')
 				continue
-			if len(evolution.moveEffectiveness.elementWeaknesses) == 0:
-				print(evolution.evolutionSaveName, ' has no weaknesses')
+			if len(evolution.moveEffectiveness.elementWeaknesses) == 0 and len(evolution.moveEffectiveness.elementResistances) == 0:
+				print(evolution.evolutionSaveName, ' has no element weaknesses/resistances')
 			else:
-				var printStr = evolution.evolutionSaveName + ' is weak to '
-				for weakness: Move.Element in evolution.moveEffectiveness.elementWeaknesses:
-					printStr += Move.element_to_string(weakness) + ' '
-				print(printStr)
+				if len(evolution.moveEffectiveness.elementWeaknesses) > 0:
+					var printStr = evolution.evolutionSaveName + ' is weak to '
+					for weakness: Move.Element in evolution.moveEffectiveness.elementWeaknesses:
+						printStr += Move.element_to_string(weakness) + ' '
+					print(printStr)
+				else:
+					print(evolution.evolutionSaveName, ' has no element weaknesses')
+				if len(evolution.moveEffectiveness.elementResistances) > 0:
+					var printStr = evolution.evolutionSaveName + ' element resists '
+					for resistance: Move.Element in evolution.moveEffectiveness.elementResistances:
+						printStr += Move.element_to_string(resistance) + ' '
+					print(printStr)
+				else:
+					print(evolution.evolutionSaveName, ' has no element resistances')
+
+func print_combatant_status_resistances(combatant: Combatant):
+	if combatant.moveEffectiveness == null:
+		print(combatant.save_name(), ' has no effectiveness data')
+		return
+	
+	if len(combatant.moveEffectiveness.statusResistances) == 0 and len(combatant.moveEffectiveness.statusImmunities) == 0:
+		print(combatant.save_name(), ' has no status resistances/immunities')
+	else:
+		if len(combatant.moveEffectiveness.statusResistances) > 0:
+			var printStr: String = combatant.save_name() + ' resists '
+			for statusType: StatusEffect.Type in combatant.moveEffectiveness.statusResistances:
+				printStr += StatusEffect.status_type_to_string(statusType) + ' '
+			print(printStr)
+		else:
+			print(combatant.save_name(), ' has no status resistances')
+		if len(combatant.moveEffectiveness.statusImmunities) > 0:
+			var printStr: String = combatant.save_name() + ' is immune to '
+			for statusType: StatusEffect.Type in combatant.moveEffectiveness.statusImmunities:
+				printStr += StatusEffect.status_type_to_string(statusType) + ' '
+			print(printStr)
+		else:
+			print(combatant.save_name(), ' has no status immunities')
+	if combatant.evolutions != null:
+		for evolution: Evolution in combatant.evolutions.evolutionList:
+			if len(evolution.moveEffectiveness.statusResistances) == 0 and len(evolution.moveEffectiveness.statusImmunities) == 0:
+				print(evolution.evolutionSaveName, ' has no status resistances/immunities')
+			else:
+				if len(evolution.moveEffectiveness.statusResistances) > 0:
+					var printStr: String = evolution.evolutionSaveName + ' resists '
+					for statusType: StatusEffect.Type in evolution.moveEffectiveness.statusResistances:
+						printStr += StatusEffect.status_type_to_string(statusType) + ' '
+					print(printStr)
+				else:
+					print(evolution.evolutionSaveName, ' has no status resistances')
+				if len(evolution.moveEffectiveness.statusImmunities) > 0:
+					var printStr: String = evolution.evolutionSaveName + ' is immune to '
+					for statusType: StatusEffect.Type in evolution.moveEffectiveness.statusImmunities:
+						printStr += StatusEffect.status_type_to_string(statusType) + ' '
+					print(printStr)
+				else:
+					print(evolution.evolutionSaveName, ' has no status immunities')
 
 func print_combatant_movepool_size(combatant: Combatant):
 	print(combatant.save_name(), ' has ', len(combatant.stats.movepool.pool), ' moves')
