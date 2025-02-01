901019f44afa0320fbae1856a2ed6e47a66f4f13
123outerme <123outerme.outlook.com>
2025-02-01 10:16:49 -0500

    Add HP bar filling when using a healing item outside of battle

--- scripts/menus/inventory_panel/item_use_panel.gd
+++ scripts/menus/inventory_panel/item_use_panel.gd
@@ -10,7 +10,13 @@ signal ok_pressed
 @onready var itemSprite: Sprite2D = get_node("Panel/ItemSpriteControl/ItemSprite")
 @onready var itemUsedTitle: RichTextLabel = get_node("Panel/ItemUsedTitle")
 @onready var itemUsedEffects: RichTextLabel = get_node("Panel/ItemUsedEffects")
+
 @onready var moveLearnAnimController: MoveLearnAnimationController = get_node('Panel/MoveLearnAnimControl')
+
+@onready var hpDisplay: Control = get_node('Panel/HpDisplay')
+@onready var hpLabel: RichTextLabel = get_node('Panel/HpDisplay/Hp')
+@onready var hpBar: TextureProgressBar = get_node('Panel/HpDisplay/HpProgressBar')
+
 @onready var okButton: Button = get_node('Panel/OkButton')
 
 func _unhandled_input(event):
@@ -24,12 +30,25 @@ func load_item_use_panel():
 		target = PlayerResources.playerInfo.combatant
 	
 	moveLearnAnimController.visible = false
+	hpDisplay.visible = false
 	if item != null:
 		itemSprite.texture = item.itemSprite
 		if target.would_item_have_effect(item):
 			itemUsedTitle.text = '[center]Item Used - ' + item.itemName + '[/center]'
 			if learnedMove == null:
 				itemUseText = item.get_as_subclass().get_use_message(target)
+				if item is Healing:
+					hpBar.max_value = target.stats.maxHp
+					hpBar.value = target.currentHp
+					hpBar.tint_progress = Combatant.get_hp_bar_color(target.currentHp, target.stats.maxHp)
+					hpDisplay.visible = true
+					
+					var healedHp: int = min(target.stats.maxHp, target.currentHp + item.healBy)
+					var hpDrainTween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_LINEAR)
+					hpDrainTween.parallel().tween_property(hpBar, 'value', healedHp, 1)
+					hpDrainTween.parallel().tween_property(hpBar, 'tint_progress', Combatant.get_hp_bar_color(healedHp, target.stats.maxHp), 1)
+					hpLabel.text = '[center]' + TextUtils.num_to_comma_string(healedHp) \
+							+ ' / ' + TextUtils.num_to_comma_string(target.stats.maxHp) + '[/center]'
 			else:
 				var shard: Shard = item as Shard
 				var combatant: Combatant = Combatant.load_combatant_resource(shard.combatantSaveName)
@@ -42,7 +61,7 @@ func load_item_use_panel():
 		else:
 			itemUsedTitle.text = '[center]Has No Effect - ' + item.itemName + '[/center]'
 			itemUseText = 'You attempted to use the ' + item.itemName + ', but it has no effect'
-			if item.itemType == Item.Type.HEALING:
+			if item is Healing:
 				itemUseText += ', because you are at full health already'
 			if item is KeyItem:
 				if item.get_as_subclass() is StatResetItem:
