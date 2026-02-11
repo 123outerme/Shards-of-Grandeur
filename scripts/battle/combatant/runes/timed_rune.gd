3cd081c720defd38ba5c2842b38e8afada0097a9
123outerme <123outerme.outlook.com>
2026-02-10 20:22:43 -0500

    Fixed timed rune issues with triggering consistently through save/reloads, etc

--- scripts/battle/combatant/runes/timed_rune.gd
+++ scripts/battle/combatant/runes/timed_rune.gd
@@ -4,7 +4,7 @@ class_name TimedRune
 ## the number of turns after the rune is put in place until it triggers (first turn being counted at the end step of the same turn)
 @export var afterTurns: int = 1
 
-@export_storage var turnCounter: int = 0
+@export_storage var turnEnchanted: int = 0
 
 func _init(
 	i_orbChange: int = 0,
@@ -19,25 +19,33 @@ func _init(
 	i_runeSpriteAnim: MoveAnimSprite = null,
 	i_triggerAnims: Array[MoveAnimSprite] = [],
 	i_afterTurns: int = 1,
-	i_turnCounter: int = 0,
+	i_turnEnchanted: int = 0,
 ):
 	super(i_orbChange, i_category, i_element, i_power, i_lifesteal, i_statChanges, i_statusEffect, i_surgeChanges, i_caster, i_runeSpriteAnim, i_triggerAnims)
 	afterTurns = i_afterTurns
-	turnCounter = i_turnCounter
+	turnEnchanted = i_turnEnchanted
+
+func init_rune_state(combatant: Combatant, otherCombatants: Array[Combatant], state: BattleState) -> void:
+	super.init_rune_state(combatant, otherCombatants, state)
+	turnEnchanted = state.turnNumber
 
 func get_rune_type() -> String:
 	return 'Timed Rune'
 
 func get_rune_trigger_description() -> String:
-	return 'After ' + String.num_int64(afterTurns - turnCounter) + ' Turns'
+	if turnEnchanted == 0:
+		return 'After ' + String.num_int64(afterTurns) + ' Turns'
+	else:
+		return 'At the end of Turn ' + String.num_int64(turnEnchanted + afterTurns - 1)
 
 func get_rune_tooltip() -> String:
 	return "This Rune's effect triggers after a set number of turns have passed."
 
 func does_rune_trigger(combatant: Combatant, otherCombatants: Array[Combatant], state: BattleState, timing: BattleCommand.ApplyTiming, firstCheck: bool) -> bool:
-	if timing == BattleCommand.ApplyTiming.AFTER_ROUND and firstCheck:
-		turnCounter += 1
-	return turnCounter >= afterTurns
+	if timing == BattleCommand.ApplyTiming.AFTER_ROUND:
+		return state.turnNumber >= turnEnchanted + afterTurns - 1 # the first After-Round phase after the Rune is enchanted is "Turn 1" of the timer, not "Turn 0"
+	else:
+		return false
 
 func copy(copyStorage: bool = false) -> TimedRune:
 	var rune: TimedRune = TimedRune.new(
@@ -56,6 +64,6 @@ func copy(copyStorage: bool = false) -> TimedRune:
 	)
 	
 	if copyStorage:
-		rune.turnCounter = turnCounter
+		rune.turnEnchanted = turnEnchanted
 	
 	return rune
